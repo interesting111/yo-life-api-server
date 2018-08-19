8a8120559ce67a9937ef5e55c997419a2bf17f99
HelloZZZC <405954049@qq.com>
2018-08-19 15:00:36 +0800

    login api

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,5 +10,16 @@ class LoginController extends BaseController
 
     public function index($request, $response, $args)
     {
+        $loginAuthUrl = $this->getWeAppProvider()->getLoginAuthUrl($request->getQueryParam('code'));
+
+        $result = $this->getGuzzleServiceProvider()->request('GET', $loginAuthUrl);
+
+        $thirdSessionKey = $this->getWeAppProvider()->encryptSessionKey($result['body']['session_key'], $result['body']['openid']);
+
+        $this->getRedis()->set('third_sessionKey', $thirdSessionKey);
+
+        return $response->withJson($this->createSuccessResponse([
+            'thirdSessionKey' => $thirdSessionKey,
+        ]));
     }
 }
